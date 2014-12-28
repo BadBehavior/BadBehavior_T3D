f946aec265ad733cff3a090a9274297d075b57a9
GuyAllard <guyallard0@gmail.com>
2014-12-28 12:02:24 +0100

    Added license headers to script files

--- Templates/Full/game/scripts/server/BadBehavior/BadBot.cs
+++ Templates/Full/game/scripts/server/BadBehavior/BadBot.cs
@@ -1,3 +1,25 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2014 Guy Allard
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//-----------------------------------------------------------------------------
+
 //=============================================================================
 // Supporting functions for an AIPlayer driven by a behavior tree
 //=============================================================================
@@ -44,8 +66,9 @@ function BadBot::spawn(%name, %startPos)
    
    %bot.tetherPoint = %startPos;
    
-   return %bot;  
-   //%bot.allowSprinting(false);
+   %bot.allowSprinting(false);
+   
+   return %bot;      
 }
 
 // override getMuzzleVector so that the bots aim at where they are looking
@@ -374,7 +397,7 @@ function shootAtTargetTask::precondition(%this, %obj)
 
 function shootAtTargetTask::behavior(%this, %obj)
 {
-   %obj.setMoveTrigger($player::imageTrigger0);
+   %obj.setImageTrigger(0, true);
 }
 
 
