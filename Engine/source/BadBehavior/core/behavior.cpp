0fb5497dc654588b821fd9d5ae319933ff23f273
GuyAllard <guyallard01@gmail.com>
2015-01-05 09:25:17 +0100

    made behavior an instantiable class so that compiled behaviors can use its icon

--- Engine/source/BadBehavior/core/behavior.cpp
+++ Engine/source/BadBehavior/core/behavior.cpp
@@ -40,6 +40,8 @@ EndImplementEnumType;
 //------------------------------------------------------------------------------
 // Behavior node
 //------------------------------------------------------------------------------
+IMPLEMENT_CONOBJECT(Behavior);
+
 Behavior::Behavior()
    : mPreconditionMode(ONCE)
 {
@@ -95,7 +97,7 @@ Task* BehaviorTask::update()
       mStatus = FAILURE;
    }
 
-   mIsComplete = mStatus != RUNNING;
+   mIsComplete = mStatus != RUNNING && mStatus != SUSPENDED;
 
    if(mIsComplete)
       node->onExit(mOwner);
