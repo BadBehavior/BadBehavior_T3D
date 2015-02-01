3f89ad3faf307eb82a4665e7865128367d8ce18f
GuyAllard <guyallard01@gmail.com>
2015-02-01 09:05:06 +0100

    added comments to wanderTree

--- Templates/Full/game/scripts/server/BadBehavior/behaviorTrees/WanderTree.cs
+++ Templates/Full/game/scripts/server/BadBehavior/behaviorTrees/WanderTree.cs
@@ -9,18 +9,22 @@ new Root(WanderTree) {
 
       new ScriptedBehavior() {
          preconditionMode = "ONCE";
+         internalName = "move somewhere";
          class = "wanderTask";
          canSave = "1";
          canSaveDynamicFields = "1";
       };
       new WaitForSignal() {
          signalName = "onReachDestination";
+         timeoutMs = "0";
+         internalName = "wait until there";
          canSave = "1";
          canSaveDynamicFields = "1";
       };
       new RandomWait() {
          waitMinMs = "0";
          waitMaxMs = "1000";
+         internalName = "pause";
          canSave = "1";
          canSaveDynamicFields = "1";
       };
